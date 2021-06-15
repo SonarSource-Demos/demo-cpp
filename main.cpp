d0e103b2bfc38288b615ff91ea8bff937e7ca70b
Brian Cipollone <brian@bigoniontech.com>
2021-06-14 21:48:50 -0500

    add mvval func

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ int main()
 {
     int values[5] = {1,2,3,4,5};
     double avg = getAverage(values, 10);
+    std::cout << avg;
+}
+
+void mvval(){
     char array[10];
     void *pos = memchr(array, '@', 42);
-    std::cout << avg;
 }
 
